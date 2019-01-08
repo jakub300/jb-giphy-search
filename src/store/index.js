0c00fcba78ea8c2db2323236cf674352466b9a77
Jakub Bogucki <jakub300@gmail.com>
2019-01-08 02:28:31 +0100

    Persist vuex state

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,18 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 
 import favorites from './modules/favorites';
 
 Vue.use(Vuex);
 
+const vuexLocal = new VuexPersistence();
+
 const store = new Vuex.Store({
   modules: {
     favorites,
   },
+  plugins: [vuexLocal.plugin],
   strict: process.env.NODE_ENV !== 'production',
 });
 
